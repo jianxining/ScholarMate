b0a7ed6fbde92efe7394ff252303271ad1cd183d
tmengchun <tmengchun@163.com>
2026-04-24 11:09:53 +0800

    init

---
+++ src/main/java/cn/mc/agent/ScholarMateApplication.java
@@ -0,0 +1,21 @@
+package cn.mc.agent;
+
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+/**
+ * @author tmengchun
+ * @description
+ * @create 2026/4/24 10:49
+ */
+
+@SpringBootApplication
+public class ScholarMateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScholarMateApplication.class, args);
+    }
+}
